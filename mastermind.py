35e12e0339c51645d213eaaa9ec1d146db3716c3
Mica <mika89700@gmail.com>
2020-01-13 20:36:23 +0100

    add main py

---
+++ mastermind.py
@@ -0,0 +1,11 @@
+# MASTERMIND
+# Definition des boules et du plateau
+essai = 10
+
+# y = yellow ; b = blue ; g = green ; r = red ; w = white ; p = purple
+
+# Creation du code
+# Jeu
+# Check result
+# Graph
+
