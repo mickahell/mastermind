7268b0420d2ebcaf29b4e32161a3b56ad0d1e046
Mica <mika89700@gmail.com>
2020-02-01 15:42:20 +0100

    edit nb tour + add option for machine and todo list

--- mastermind.py
+++ mastermind.py
@@ -1,18 +1,20 @@
 # MASTERMIND
 # Règles : 12 chances, 5 billes, 8 couleurs
+# JOueur VS machine --> DONE
+# TODO --> Player is machine
 
 import random
 
 # y = yellow ; b = blue ; g = green ; r = red ; w = white ; p = purple ; i = invisible ; o = orange
 couleurs = ['y', 'b', 'g', 'r', 'w', 'p', 'i', 'o']
-tour = 2
+tour = 12
 
-humain = 1
-partie = 1
+humain = True
+partie = True
 
 #player = str(input("Qui décode, une machine ou un humain ? machine/humain : "))
 #if player != "humain":
-#    machine = 1
+#    humain = False
 
 ################## Creation du code
 # Random si codeur is machine
@@ -23,23 +25,27 @@ while len(code) < 5:
     code.insert(i, sample)
     i += 1
 
-while partie == 1:
+while partie:
     ################ Proposition du joueur / machine
     print("#############################################")
-    print("y = yellow ; b = blue ; g = green ; r = red ; w = white ; p = purple ; i = invisible ; o = orange")
     proposition = []
-    x = 0
-    while len(proposition) < 5:
-        sample = str(input("Choisie une couleur : "))
-        proposition.insert(x, sample)
-        x += 1
+    # Joueur est un humain
+    if humain:
+        print("y = yellow ; b = blue ; g = green ; r = red ; w = white ; p = purple ; i = invisible ; o = orange")
+        x = 0
+        while len(proposition) < 5:
+            sample = str(input("Choisie une couleur : "))
+            proposition.insert(x, sample)
+            x += 1
+    # Joueur est une machine
+
 
     ################## Check result
     if code == proposition:
         print("%s = %s" % (proposition, code))
         print("#################################")
         print("You wiiiiin !")
-        partie = 0
+        partie = False
     else:
         result = []
         restecode = [] # Méthode de matheux, comme avec une division
@@ -66,7 +72,7 @@ while partie == 1:
         tour -= 1
         print("Tours restant : ", tour)
         if tour == 0:
-            partie = 0
+            partie = False
             print("#############################################")
             print("You lose !!!")
 # Graph
